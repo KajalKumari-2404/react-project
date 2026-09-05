83d0bfb958c193b6bfcffdc2413c58466af3595f
Kajal Kumari <kajaldob06@gmail.com>
2026-09-06 00:49:10 +0530

    create a real-time digital clock in react-display current date and time in todo app.

--- react-project/src/projects/Todo/Todo.jsx
+++ react-project/src/projects/Todo/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MdCheck, MdDeleteForever } from "react-icons/md";
 
 import "./Todo.css";
@@ -6,6 +6,7 @@ import "./Todo.css";
 export const Todo = () => {
     const [inputValue, setInputValue] = useState("");
     const [task, setTask] = useState([]);
+    const [dateTime, setDateTime] = useState("");
 
     const handleInputChange = (value) => {
         setInputValue(value);
@@ -26,14 +27,25 @@ export const Todo = () => {
         setInputValue("");
     };
 
+    //todo date and time
+    useEffect(() => {
+    const interval = setInterval(() => {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString();
+    const formattedTime = now.toLocaleTimeString();
+
+    setDateTime(`${formattedDate} - ${formattedTime}`);
+    }, 1000);
+    return () => clearInterval(interval);
+    }, []);
+
+
   return (
     <section className='todo-container'>
-        {/* <header>
-            <h1>Todo List</h1>
-        </header> */}
-
+        
         <header className="header">
     <h1>Todo List</h1>
+    <h2 className='date-time'>{dateTime}</h2>
 </header>
         <section className='form'>
             <form onSubmit={handleFormSubmit}>
