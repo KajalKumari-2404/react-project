0f46d51605a0180c4279ab2a9b86388540a70c41
Kajal Kumari <kajaldob06@gmail.com>
2026-09-05 14:28:35 +0530

    Best website to find Icons for react app.

--- react-project/src/projects/ToggleSwitch/ToggleSwitch.jsx
+++ react-project/src/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import "./ToggleSwitch.css";
+import { IoIosSwitch } from "react-icons/io";
+import { PiUserSwitchFill } from "react-icons/pi";
+
+
 
 export const ToggleSwitch = () => {
     const [isOn, setIsOn] = useState(false);
@@ -12,6 +16,12 @@ export const ToggleSwitch = () => {
     const toggleBGColor = { backgroundColor: isOn ? "#4caf50" : "#f44336" }
 
   return (
+    <>
+    <h1 style={{ color: "#000", textAlign: "center" }}>
+        Toggle Switch{" "}
+        <IoIosSwitch style={{ color: "red", textAlign: "center" }}/>
+        <PiUserSwitchFill />
+        </h1>
     <div className='toggle-switch'
     style={toggleBGColor}
      onClick={handleToggleSwitch}>
@@ -19,5 +29,6 @@ export const ToggleSwitch = () => {
             <span className='switch-state'>{checkIsOn}</span>
         </div>
     </div>
+    </>
   )
 }
