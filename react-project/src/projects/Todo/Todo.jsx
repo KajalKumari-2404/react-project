c3b0d9432b2c56f173f9dadf44dc185dfa5f6fad
Kajal Kumari <kajaldob06@gmail.com>
2026-09-07 00:58:13 +0530

    Delete elements and auto-update tasks in react : add a clear all button on Todo App.

--- react-project/src/projects/Todo/Todo.jsx
+++ react-project/src/projects/Todo/Todo.jsx
@@ -39,6 +39,20 @@ export const Todo = () => {
     return () => clearInterval(interval);
     }, []);
 
+    // Todo handleDeleteTodo function
+
+    const handleDeleteTodo = (value) => {
+        console.log(task);
+        console.log(value);
+        const updatedTask = task.filter((curTask) => curTask !== value);
+        setTask(updatedTask);
+    };
+
+    //todo handleClearTodoData functionality
+    const handleClearTodoData = () => {
+        setTask([]);
+    };
+
 
   return (
     <section className='todo-container'>
@@ -72,13 +86,16 @@ export const Todo = () => {
                             <button className='check-btn'>
                                 <MdCheck/>
                             </button>
-                            <button className='delete-btn'>
+                            <button className='delete-btn' onClick={() => handleDeleteTodo(curTask)}>
                                 <MdDeleteForever/>
                             </button>
                         </li>
                     })
                 }
             </ul>
+        </section>
+        <section>
+            <button className='clear-btn' onClick={handleClearTodoData}>Clear all</button>
         </section>
     </section>
   );
