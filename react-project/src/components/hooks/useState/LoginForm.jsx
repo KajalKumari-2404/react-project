e853802a33c95b018eb6bb8b36ac673f88065cc1
Kajal Kumari <kajaldob06@gmail.com>
2026-09-10 00:19:29 +0530

    login form in react.

--- react-project/src/components/hooks/useState/LoginForm.jsx
+++ react-project/src/components/hooks/useState/LoginForm.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import "./index.css";
 
 export const LoginForm = () => {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const loginData = {
+        username,
+        password,
+    };
+    console.log(loginData);
+};
+
     return (
         <div className="container">
             <div className="card">
                 <h1>Login Form</h1>
 
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <label htmlFor="username">
                         Username
                     </label>
@@ -16,6 +31,8 @@ export const LoginForm = () => {
                         name="username"
                         required
                         autoComplete="off"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                     />
 
                     <label htmlFor="password">
@@ -27,6 +44,8 @@ export const LoginForm = () => {
                         name="password"
                         required
                         autoComplete="off"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
 
                     <button type="submit">
